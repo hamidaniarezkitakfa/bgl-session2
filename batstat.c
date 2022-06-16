a3560acdc4a19728f982219717d5c0b6adf249e4
hamidaniarezkitakfa <arezkitakfa@gmail.com>
2022-06-16 16:35:48 +0200

    ajoute de la fonction moyenne

--- batstat.c
+++ batstat.c
@@ -5,9 +5,17 @@
 
 //fonction moyen
 
-static double  moy () {
+static double  moy (double ,m double p_ch , double p ,double longth ) { // m la moyenne , p poid total  , p_ch poid dune chauve souri, longth est la taille de notre fichier csv 
+int temp ;// chaque fois que en trouve un poid en incremente temp a la fin  de la boucle temp retourne le nombre de chauve souri trouver 
+for(int i=0; i<longth; i++){
+  if {//nom de famille entre corespendant a la case ou il pointe i 
+    
+  p = p+ p_ch ;
+  temp = temp +1;
+}
+}
 
-
+m = p/temp; 
 
 
 static double  ecart () {
